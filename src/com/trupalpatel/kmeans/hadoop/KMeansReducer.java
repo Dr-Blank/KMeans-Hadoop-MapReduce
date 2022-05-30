e77817b312fdf6064aff1e4d8c623c21c4ec5332
Dr-Blank <64108942+Dr-Blank@users.noreply.github.com>
2022-05-29 22:20:17 -0400

    package changed

---
+++ src/com/trupalpatel/kmeans/hadoop/KMeansReducer.java
@@ -0,0 +1,31 @@
+package com.trupalpatel.kmeans.hadoop;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import it.unipi.hadoop.model.Point;
+
+public class KMeansReducer extends Reducer<IntWritable, Point, Text, Text> {
+
+    private final Text centroidId = new Text();
+    private final Text centroidValue = new Text();
+
+    public void reduce(IntWritable centroid, Iterable<Point> partialSums, Context context)
+            throws IOException, InterruptedException {
+
+        // Sum the partial sums
+        Point sum = Point.copy(partialSums.iterator().next());
+        while (partialSums.iterator().hasNext()) {
+            sum.sum(partialSums.iterator().next());
+        }
+        // Calculate the new centroid
+        sum.average();
+
+        centroidId.set(centroid.toString());
+        centroidValue.set(sum.toString());
+        context.write(centroidId, centroidValue);
+    }
+}
